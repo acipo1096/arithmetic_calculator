342f8894320e36958326d2c3616cbb911da4c66c
Alex Cipollone <alex.cipollone10@gmail.com>
2023-05-10 12:58:13 -0400

    Only need to get two more tests to work

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -21,11 +21,11 @@ def arithmetic_arranger(problems):
   # Now, convert the list to a string
   # AND NOW YOU NEED TO REMOVE ALL NON-DIGITS
   string_list = problemA
-  stringC = ' '.join([str(item) for item in string_list])
-  print("This is a string: ", stringC)
-  stringB = re.sub('[^0-9+-\/\*]', ' ', stringC)
-  stringA = stringB
-  print("This is a regex string: ", stringA)
+  stringA = ' '.join([str(item) for item in string_list])
+  # print("This is a string: ", stringC)
+  # stringB = re.sub('[^0-9+-\/\*]', ' ', stringC)
+  # stringA = stringB
+  # print("This is a regex string: ", stringA)
 
   space = ""
   problemSpace = "    "
@@ -43,14 +43,34 @@ def arithmetic_arranger(problems):
       splitA.append(ele)
   print(splitA)
 
-  problemResult = ''
+  # Check for non-letters
+  regex = re.compile("[0-9\+\-\*\/]+")
+  for r in range(0, len(splitA), 1):
+    print("The current iteration value is: ", splitA[r])
+    result = regex.match(splitA[r]), 0, len(splitA[r])
+    # Matches the matched string as opposed to the object returned by .match()
+    if (result[0][0] != splitA[r]):
+      # print(splitA[r])
+      return 'Error: Numbers must only contain digits.'
 
   # What if I keep the same coding principle but alter the syntax/code used?
   for i in range(0, len(splitA) - 1, 3):
+    print("i+3 is: ", i + 3)
+    print("Length is: ", len(splitA))
+
     # Now, we have to return error if * or /
     if (splitA[i + 1] == '*' or splitA[i + 1] == '/'):
-      print('Error: Operator must be \'+\' or \'-\'')
-      continue
+      return "Error: Operator must be \'+\' or \'-\'."
+
+    # Check for too many operands
+    if (i > 12):
+      return "Error: Too many problems."
+
+    # Check for too many digits
+    print("The length of splitA[i+2] is: ", len(splitA[i + 2]))
+    print("splitA is: ", splitA[i + 2])
+    if (len(splitA[i]) > 4 or len(splitA[i + 2]) > 4):
+      return "Error: Numbers cannot be more than four digits."
 
     if (len(splitA[i]) > len(splitA[i + 2])):
       spaceLength = len(splitA[i]) - len(splitA[i + 2])
@@ -62,13 +82,22 @@ def arithmetic_arranger(problems):
       splitA[i + 2] = splitA[i + 1] + " " + space + splitA[i + 2]
       space = ""
 
-      lineOne += splitA[i] + problemSpace
-      lineTwo += splitA[i + 2] + problemSpace
+      if (i + 3 < len(splitA) - 1):
+        lineOne += splitA[i] + problemSpace
+      else:
+        lineOne += splitA[i]
+
+      if (i + 3 < len(splitA) - 1):
+        lineTwo += splitA[i + 2] + problemSpace
+      else:
+        lineTwo += splitA[i + 2]
 
       for k in range(0, len((splitA[i])), 1):
+        print("k is: ", k)
         dashedLine += "-"
 
-      dashedLine += problemSpace
+      if (i + 3 < len(splitA)):
+        dashedLine += problemSpace
 
     else:
       spaceLength = len(splitA[i + 2]) - len(splitA[i])
@@ -79,19 +108,27 @@ def arithmetic_arranger(problems):
       splitA[i + 2] = splitA[i + 1] + " " + splitA[i + 2]
       space = ""
 
-      lineOne += splitA[i] + problemSpace
-      lineTwo += splitA[i + 2] + problemSpace
+      if (i + 3 < len(splitA) - 1):
+        lineOne += splitA[i] + problemSpace
+      else:
+        lineOne += splitA[i]
+
+      if (i + 3 < len(splitA) - 1):
+        lineTwo += splitA[i + 2] + problemSpace
+      else:
+        lineTwo += splitA[i + 2]
 
       for k in range(0, len((splitA[i + 2])), 1):
+        print("k is: ", k)
         dashedLine += "-"
 
-      dashedLine += problemSpace
-
-  print(" " + lineOne, '\n', lineTwo, '\n', dashedLine)
-
+      if (i + 3 < len(splitA)):
+        dashedLine += problemSpace
   # Puts the arranged_problems return on the next line
   # print()
   # print(res[1])
 
-  arranged_problems = 0
+  arranged_problems = str(lineOne) + '\n' + str(lineTwo) + '\n' + dashedLine
+  print(repr(arranged_problems))
+
   return arranged_problems
